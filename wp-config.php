b95fcafb7e6de72ffb79114e12f6dff22152f62f
Bill <bill.phelps@ntlworld.com>
2016-01-02 12:08:22 +0000

    everything tracked + wp-config moved

---
+++ wp-config.php
@@ -0,0 +1,6 @@
+<?php
+
+/** Location of your WordPress configuration. */
+require_once(ABSPATH . '../../../private/vagrant-2_wp-config.php');
+
+// NB: ABSPATH is defined in wp-load.php as the cms folder
