3e44755355a288d7040ab347a9be300d4fa619c0
Bill <bill.phelps@ntlworld.com>
2016-01-03 20:57:17 +0000

    fix contact form css + tbpull

---
+++ wp-content/themes/wfs2_child1/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// FUNCTIONS.PHP - CHILD THEME VERSION: Loads before parent theme functions.php
+
+// to override styles in parent theme
+//wordpress.org/support/topic/plugin-wordpress-seo-by-yoast-sitemap-autodiscovery-validation-problems
+add_action('wp_enqueue_scripts', function() {
+	wp_enqueue_style('child_css',get_stylesheet_directory_uri().'/css/child.css');
+}); 
