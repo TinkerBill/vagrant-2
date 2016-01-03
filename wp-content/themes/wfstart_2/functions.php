466bb2c8d3f33c8d842b8d39f926c763e63a01bd
Bill <bill.phelps@ntlworld.com>
2016-01-03 19:15:36 +0000

    fix validation error with yoast breadcrumbs

--- wp-content/themes/wfstart_2/functions.php
+++ wp-content/themes/wfstart_2/functions.php
@@ -287,6 +287,13 @@ function lib_move_stuff() { // stuff that broke when I moved wf_lib to a plugin
 		);
 		return implode(',',array_keys($excludes));
 	}
+	
+	// fixes validation error with yoast breadcrumbs
+	add_filter ('wpseo_breadcrumb_output','mc_microdata_breadcrumb');
+	function mc_microdata_breadcrumb ($link_output) {
+		$link_output = preg_replace(array('#<span xmlns:v="http://rdf.data-vocabulary.org/\#">#','#<span typeof="v:Breadcrumb"><a href="(.*?)" .*?'.'>(.*?)</a></span>#','#<span typeof="v:Breadcrumb">(.*?)</span>#','# property=".*?"#','#</span>$#'), array('','<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="$1" itemprop="url"><span itemprop="title">$2</span></a></span>','<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">$1</span></span>','',''), $link_output);
+		return $link_output;
+	}
 
 		
 		
